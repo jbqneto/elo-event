e759b90ffbe252edbdf6531b301f921fd4bf0bb4
José Bezerra <jbqneto@gmail.com>
2023-02-10 12:01:03 +0000

    almost ready

--- src/app/components/main/badge/badge.component.ts
+++ src/app/components/main/badge/badge.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { IMember } from 'src/app/models/member.model';
+import { QRCoder } from 'src/app/services/qr-code/qr-code.service';
+import { SessionService } from 'src/app/services/session/sesssion.service';
 
 @Component({
   selector: 'app-badge',
   templateUrl: './badge.component.html',
   styleUrls: ['./badge.component.css']
 })
-export class BadgeComponent {
+export class BadgeComponent implements OnInit, AfterViewInit {
+  public user: IMember | null = null;
+
+  constructor(private session: SessionService) {}
+
+  public ngOnInit(): void {
+      this.user = this.session.get('user') ?? null;
+  }
+
+  public ngAfterViewInit(): void {
+    console.log(this.user);
+    QRCoder.generate('qr-code', {
+      content: `${this.user?.id}`,
+      height: 128,
+      width: 128,
+    })
+  }
+
 
 }
--- src/app/components/main/map/map.component.ts
+++ src/app/components/main/map/map.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { DomSanitizer, SafeUrl } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-map',
@@ -6,5 +7,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./map.component.css']
 })
 export class MapComponent {
+  public mapUrl: string = 'https://www.google.com/maps/@-7.275722,-34.895194,16z?hl=pt-BR'
+
+  public constructor(private sanitizer: DomSanitizer) {}
+
+  public get safeMapUrl(): SafeUrl {
+
+    return this.sanitizer.bypassSecurityTrustUrl(this.mapUrl);
+  }
 
 }
