cef90765310864aae58f8f1afce98f071509edf0
José Bezerra <jbqneto@gmail.com>
2023-02-10 02:58:00 +0000

    structure on going

---
+++ src/app/components/main/home/home.component.ts
@@ -0,0 +1,34 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { IMember, Member } from 'src/app/models/member.model';
+import { SessionService } from 'src/app/services/session/sesssion.service';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent {
+  public user: string = '';
+
+  private member: Member | null = null;
+
+  public constructor(private session: SessionService, private router: Router) {
+    try {
+      const dbUser = session.get<IMember>('user');
+
+      if (dbUser === null)
+        throw new Error('User not found on session');
+
+      this.member = new Member(dbUser);
+      this.user = this.member?.getFirstName() ?? '';  
+    } catch (error) {
+      console.log(error);
+      this.session.remove('user');
+      setTimeout(() => {
+        this.router.navigate(['/login']);  
+      }, 500);
+      
+    }
+  }
+}
---
+++ src/app/components/main/main.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { IMember, Member } from 'src/app/models/member.model';
+import { SessionService } from 'src/app/services/session/sesssion.service';
+
+@Component({
+  selector: 'app-main',
+  styleUrls: ['main.component.css'],
+  templateUrl: './main.component.html',
+})
+export class MainComponent {
+  public member: Member | null = null;
+  public isAdmin: boolean = false;
+
+  constructor(private session: SessionService) {
+    const user = session.get<IMember>('user');
+
+    if (user !== null) {
+      this.member = new Member(user);
+      this.isAdmin = this.member.admin;
+    }
+  }
+
+}
